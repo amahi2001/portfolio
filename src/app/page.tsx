21cd8ac6b12e53862f2ea67d33c06147daa38eb6
Abrar <amahi2001@gmail.com>
2023-11-17 17:42:17 -0500

    work on offcanvas navbar. name row utilizing scss mediaquery rather than inane css + js

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import Button from "react-bootstrap/Button";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import NavDropdown from "react-bootstrap/NavDropdown";
+import Offcanvas from "react-bootstrap/Offcanvas";
 // import Collapse from "react-bootstrap/Collapse";
 import Fade from "react-bootstrap/Fade";
 import Container from "react-bootstrap/Container";
@@ -37,6 +38,16 @@ export default function Home() {
   //shows animated text below the headshot
   const [showAnimatedText, setShowAnimatedText] = useState(false);
   const [showDesc, setShowDesc] = useState(false);
+
+  const [showOffcanvas, setShowOffcanvas] = useState(false);
+
+  const handleCloseOffcanvas = () => setShowOffcanvas(false);
+  const handleToggleMenu = () => {
+    if (isMobile) {
+      setShowOffcanvas(!showOffcanvas);
+    }
+  };
+
   return (
     <Fragment>
       <Parallax pages={2} ref={parallax} enabled={production ? showDesc : true}>
@@ -53,34 +64,47 @@ export default function Home() {
 
         {/* LinkedIn and Github links at top */}
         <Navbar
-          // expand = "lg"
+          expand="lg"
           sticky="top"
           variant="dark"
-          className="justify-content-end semi-transparent"
+          className="semi-transparent"
         >
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Fade in={showDesc}>
-            <Navbar.Collapse id="basic-navbar-nav">
-              <Nav className="me-auto">
-                {/* <Nav.Link href="#home">About Me</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link> */}
-                <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                  <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.2">
+          <Navbar.Toggle
+            aria-controls="offcanvasNavbar-expand-lg"
+            className="me-auto"
+          />
+          <Navbar.Offcanvas
+            id="offcanvasNavbar-expand-lg"
+            aria-labelledby="offcanvasNavbarLabel-expand-lg"
+            placement="start"
+          >
+            <Offcanvas.Header closeButton>
+              <Offcanvas.Title id="offcanvasNavbarLabel-expand-lg">
+                Offcanvas
+              </Offcanvas.Title>
+            </Offcanvas.Header>
+            <Offcanvas.Body className="me-auto">
+              <Nav className="justify-content-end flex-grow-1 pe-3">
+                <Nav.Link href="#action1">Home</Nav.Link>
+                <Nav.Link href="#action2">Link</Nav.Link>
+                <NavDropdown
+                  title="Dropdown"
+                  id="offcanvasNavbarDropdown-expand-lg"
+                >
+                  <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+                  <NavDropdown.Item href="#action4">
                     Another action
                   </NavDropdown.Item>
-                  <NavDropdown.Item href="#action/3.3">
-                    Something
-                  </NavDropdown.Item>
                   <NavDropdown.Divider />
-                  <NavDropdown.Item href="#action/3.4">
-                    Separated link
+                  <NavDropdown.Item href="#action5">
+                    Something else here
                   </NavDropdown.Item>
                 </NavDropdown>
               </Nav>
-            </Navbar.Collapse>
-          </Fade>
+            </Offcanvas.Body>
+          </Navbar.Offcanvas>
 
+          {/* github and linkedin icons */}
           <Navbar.Brand target="_blank" href={links.github}>
             <FontAwesomeIcon icon={faGithub} size="2x" />
           </Navbar.Brand>
@@ -101,11 +125,7 @@ export default function Home() {
         >
           <Container>
             {/* Name and Greeting */}
-            <Row
-              style={{
-                marginTop: isMobile ? "30%" : "5%",
-              }}
-            >
+            <Row className="name-row">
               <Col className="text-center">
                 <TypeAnimation
                   cursor={false}
